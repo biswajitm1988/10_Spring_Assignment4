b236658cb127220b553c0adf3ac87a058b46c801
Biswajit Mohapatra <Biswajit.Mohapatra@rci.com>
2019-09-13 12:12:13 -0400

    updates

--- src/main/java/com/fsd/spring/repository/BookRepository.java
+++ src/main/java/com/fsd/spring/repository/BookRepository.java
@@ -2,6 +2,7 @@ package com.fsd.spring.repository;
 
 import com.fsd.spring.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,6 +15,7 @@ public interface BookRepository extends JpaRepository<Book, String> {
     @Query("select b from Book b where lower(title) like %:bookTitle%")
     List<Book> findByTitleContaining(@Param("bookTitle") String bookTitle);
 
+    @Modifying
     @Query("delete from Book where lower(title) like %:bookTitle%")
     int deleteBooksByTitleContaining(@Param("bookTitle") String bookTitle);
 
